0c3ed80da399e2a51c424d095c54f617fac693dd
Jakub Panek <me@panekj.dev>
2024-02-23 16:10:43 +0100

    0.0.1+terraform-ls.0.23.7

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use std::{
 use anyhow::{anyhow, Result};
 use lapce_plugin::{
   psp_types::{
-    lsp_types::{request::Initialize, DocumentFilter, InitializeParams, Url},
+    lsp_types::{
+      request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, MessageType, Url,
+    },
     Request,
   },
   register_plugin, Http, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
@@ -20,24 +22,28 @@ struct State {}
 
 register_plugin!(State);
 
+macro_rules! error {
+  ($x:expr) => {
+    PLUGIN_RPC.window_log_message(MessageType::ERROR, $x)?;
+  };
+}
+
+macro_rules! info {
+  ($x:expr) => {
+    PLUGIN_RPC.window_log_message(MessageType::INFO, $x)?;
+  };
+}
+
 macro_rules! string {
   ( $x:expr ) => {
     String::from($x)
   };
 }
 
-macro_rules! ok {
-  ( $x:expr ) => {
-    match ($x) {
-      | Ok(v) => v,
-      | Err(e) => return Err(anyhow!(e)),
-    }
-  };
-}
+const TERRAFORM_LS_VERSION: &str = "0.32.7";
 
 fn initialize(params: InitializeParams) -> Result<()> {
-  let mut terraform_version = "0.29.1";
-  let document_selector = vec![
+  let document_selector: DocumentSelector = vec![
     DocumentFilter {
       language: Some(string!("terraform")),
       pattern: Some(string!("**/*.tf")),
@@ -49,15 +55,17 @@ fn initialize(params: InitializeParams) -> Result<()> {
       scheme: None,
     },
   ];
-  let mut server_args = vec![string!("serve")];
 
-  if let Some(options) = params.initialization_options.as_ref() {
-    if let Some(volt) = options.get("volt") {
+  let mut terraform_ls_version = string!(TERRAFORM_LS_VERSION);
+  let mut server_args = vec![string!("serve")];
+  let mut options = None;
+
+  if let Some(opts) = params.initialization_options.as_ref() {
+    options = opts.get("terraform-ls").map(|k| k.to_owned());
+
+    if let Some(volt) = opts.get("volt") {
       if let Some(args) = volt.get("serverArgs") {
         if let Some(args) = args.as_array() {
-          if !args.is_empty() {
-            server_args = vec![];
-          }
           for arg in args {
             if let Some(arg) = arg.as_str() {
               server_args.push(string!(arg));
@@ -69,22 +77,20 @@ fn initialize(params: InitializeParams) -> Result<()> {
       if let Some(server_path) = volt.get("serverPath") {
         if let Some(server_path) = server_path.as_str() {
           if !server_path.is_empty() {
-            let server_uri = ok!(Url::parse(&format!("urn:{}", server_path)));
-            PLUGIN_RPC.start_lsp(
-              server_uri,
-              server_args,
-              document_selector,
-              params.initialization_options,
-            );
+            let Ok(server_uri) = Url::parse(&format!("urn:{server_path}")) else {
+              return Err(anyhow!("Failed to parse URL"));
+            };
+            PLUGIN_RPC.start_lsp(server_uri, server_args, document_selector, options)?;
             return Ok(());
           }
         }
       }
 
-      if let Some(ver) = options.get("terraformVersion") {
-        if let Some(ver) = ver.as_str() {
-          if !ver.is_empty() {
-            terraform_version = ver
+      if let Some(tf_ls) = volt.get("terraformlsVersion") {
+        if let Some(tf_ls) = tf_ls.as_str() {
+          let tf_ls = tf_ls.trim();
+          if !tf_ls.is_empty() {
+            terraform_ls_version = string!(tf_ls)
           }
         }
       }
@@ -100,11 +106,11 @@ fn initialize(params: InitializeParams) -> Result<()> {
   };
 
   let zip_file = match VoltEnvironment::operating_system().as_deref() {
-    | Ok("macos") => format!("terraform-ls_{terraform_version}_darwin_{arch}.zip"),
-    | Ok("linux") => format!("terraform-ls_{terraform_version}_linux_{arch}.zip"),
-    | Ok("windows") => format!("terraform-ls_{terraform_version}_windows_{arch}.zip"),
-    | Ok("openbsd") => format!("terraform-ls_{terraform_version}_openbsd_{arch}.zip"),
-    | Ok("freebsd") => format!("terraform-ls_{terraform_version}_freebsd_{arch}.zip"),
+    | Ok("macos") => format!("terraform-ls_{terraform_ls_version}_darwin_{arch}.zip"),
+    | Ok("linux") => format!("terraform-ls_{terraform_ls_version}_linux_{arch}.zip"),
+    | Ok("windows") => format!("terraform-ls_{terraform_ls_version}_windows_{arch}.zip"),
+    | Ok("openbsd") => format!("terraform-ls_{terraform_ls_version}_openbsd_{arch}.zip"),
+    | Ok("freebsd") => format!("terraform-ls_{terraform_ls_version}_freebsd_{arch}.zip"),
     | Ok(v) => return Err(anyhow!("Unsupported OS: {}", v)),
     | Err(e) => return Err(anyhow!("Error OS: {}", e)),
   };
@@ -114,7 +120,7 @@ fn initialize(params: InitializeParams) -> Result<()> {
   let zip_file = PathBuf::from(zip_file);
 
   let download_url = format!(
-    "https://github.com/hashicorp/terraform-ls/releases/download/v{terraform_version}/{}",
+    "https://releases.hashicorp.com/terraform-ls/{terraform_ls_version}/{}",
     zip_file.display()
   );
 
@@ -131,6 +137,7 @@ fn initialize(params: InitializeParams) -> Result<()> {
     PLUGIN_RPC.stderr(&format!("STATUS_CODE: {:?}", resp.status_code));
     if resp.status_code.is_success() {
       let body = resp.body_read_all()?;
+
       fs::write(&zip_file, body)?;
 
       let mut zip = ZipArchive::new(File::open(&zip_file)?)?;
@@ -145,9 +152,9 @@ fn initialize(params: InitializeParams) -> Result<()> {
         if (*file.name()).ends_with('/') {
           fs::create_dir_all(&outpath)?;
         } else {
-          if let Some(p) = outpath.parent() {
-            if !p.exists() {
-              fs::create_dir_all(&p)?;
+          if let Some(path) = outpath.parent() {
+            if !path.exists() {
+              fs::create_dir_all(path)?;
             }
           }
           let mut outfile = File::create(&outpath)?;
@@ -156,18 +163,28 @@ fn initialize(params: InitializeParams) -> Result<()> {
       }
     }
 
-    fs::remove_file(&zip_file)?;
+    if let Err(e) = fs::remove_file(&zip_file) {
+      error!(format!(
+        "Failed to remove download artifact! L: {} C: {} e: {e}",
+        line!(),
+        column!()
+      ));
+    };
   }
 
   let volt_uri = VoltEnvironment::uri()?;
-  let server_uri = Url::parse(&volt_uri)?.join(server_path.to_str().unwrap_or("./terraform-ls"))?;
 
-  PLUGIN_RPC.start_lsp(
-    server_uri,
-    server_args,
-    document_selector,
-    params.initialization_options,
-  );
+  let server_path = match server_path.to_str() {
+    | Some(v) => v,
+    | None => return Err(anyhow!("server_path.to_str() failed")),
+  };
+  let Ok(server_uri) = Url::parse(&volt_uri) else {
+    return Err(anyhow!("Failed to parse URL!"));
+  };
+  let server_uri = server_uri.join(server_path)?;
+
+  info!(format!("Starting LSP server with URI: {server_uri}"));
+  PLUGIN_RPC.start_lsp(server_uri, server_args, document_selector, options)?;
 
   Ok(())
 }
@@ -179,8 +196,9 @@ impl LapcePlugin for State {
       | Initialize::METHOD => {
         let params: InitializeParams = serde_json::from_value(params).unwrap();
         if let Err(e) = initialize(params) {
-          PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
-        }
+          let _ = PLUGIN_RPC.window_log_message(MessageType::ERROR, e.to_string());
+          let _ = PLUGIN_RPC.window_show_message(MessageType::ERROR, e.to_string());
+        };
       }
       | _ => {}
     }
